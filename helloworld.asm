910b78db066f657a6d698109d517f8fbb8dde347
prasad <prasadzore31@gmail.com>
2021-03-16 21:10:27 +0530

    First program

---
+++ helloworld.asm
@@ -0,0 +1,19 @@
+section .data 
+
+msg db "Hello World Program"
+msg_len equ $-msg 
+
+section .text
+
+global _start
+_start:
+
+mov rax, 1
+mov rdi, 1
+mov rsi, msg
+mov rdx, msg_len
+syscall 
+
+mov rax, 60
+mov rdi,0
+syscall
